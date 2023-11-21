e286d8010229a7d3b23a83893aa87ad6d8da6988
kayra <kayrakai8@gmail.com>
2023-11-21 12:48:40 +0100

    boolean

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ class App
 {
     runApplication()
     {
+        let Variableswdar = 387
+        let appNaam = "i dont need no"
+        let versie = "apetrots"
+        let versiedatum = "23-234-2049"
+        let auther = "Bas Bijl"
+        let copyright = "ja toch niet stelen"
+        let distributeur = "je weet zelf"
+        let darkmode = true 
         console.log("hello world!");
+        console.log("Variabelwaarde = " + Variableswdar)
+        console.log(appNaam)
+        console.log(versie)
+        console.log(versiedatum)
+        console.log(auther)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
     }
 }
 
